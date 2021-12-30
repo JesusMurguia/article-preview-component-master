406bce7f38b11b730b21bf12cce3e8c374415d35
Jesus Murguia <jesusmurguiafac@gmail.com>
2021-12-29 20:31:34 -0700

    yeaskdfczx

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
-    var elem = '<span>SHARE</span><div class="michelle-avatar"></div>';
-
-
+    var elem = '<div id="active-desktop-author-info" class="col-12 author-info-desktop-active-state"> <div class="row share-row"> <div class="col-3 share-text">SHARE</div> <div class="col-8 d-flex"> <div class="facebook-button"></div> <div class="twitter-button"></div> <div class="pinterest-button"></div> </div> </div> </div>';
     $('[data-bs-toggle="popover"]').popover({animation:true, content:elem, html:true});  
+
+    var normalAuthorInfo = document.getElementById('normal-author-info');
+    var activeAuthorInfo = document.getElementById('active-author-info');
+    var normalButton = document.getElementById('mobile-share-button');
+
+    var activeButton = document.getElementById('mobile-share-button-active');
+    normalButton.addEventListener('click', function(e) {
+        e.preventDefault();
+            normalAuthorInfo.style.display = 'none';
+            activeAuthorInfo.style.display = 'block';
+        });
+
+    activeButton.addEventListener('click', function(e) {
+        e.preventDefault();
+            normalAuthorInfo.style.display = 'block';
+            activeAuthorInfo.style.display = 'none';
+        });
 
   });
